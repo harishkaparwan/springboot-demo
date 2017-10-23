58281ed1143c8590325bcb6a18f9fd3cdcae32ae
Harish Kaparwan <harish.kaparwan@gmail.com>
2017-10-23 11:30:18 +0530

    first commit

---
+++ HCKSpringBoot/src/main/java/com/hck/boot/controller/WelcomeController.java
@@ -0,0 +1,29 @@
+package com.hck.boot.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	// inject via application.properties
+	@Value("${welcome.message:test}")
+	private String message = "Hello World";
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "welcome";
+	}
+	@RequestMapping("/hck")
+	public String welcomehck(Map<String, Object> model) {
+		System.out.println("$$$$$$$$$$$$$$$$$$$$");
+		model.put("message", this.message);
+		return "welcome";
+	}
+}
