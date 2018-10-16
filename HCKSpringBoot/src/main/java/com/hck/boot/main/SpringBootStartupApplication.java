4e797743072f25f0844cfd1656ac1d12000790db
Harish <Harish@10.0.0.19>
2018-10-15 21:45:12 -0400

    Spring Boot 2.0.5

--- HCKSpringBoot/src/main/java/com/hck/boot/main/SpringBootStartupApplication.java
+++ HCKSpringBoot/src/main/java/com/hck/boot/main/SpringBootStartupApplication.java
@@ -1,24 +1,14 @@
 package com.hck.boot.main;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@EnableAutoConfiguration
 @ComponentScan(basePackages="com.hck.boot.controller")
-public class SpringBootStartupApplication extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootStartupApplication.class);
-	}
-
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SpringBootStartupApplication.class, args);
-	}
-
+public class SpringBootStartupApplication {
+    public static void main(String[] args) {
+    	SpringApplication.run(SpringBootStartupApplication.class, args);
+    }
 }
